710129e8ac4a75ee100b99f8e4cf945ba8032af9
Yoan F <feurtet.yoan@gmail.com>
2022-04-06 15:47:46 +0200

    fix-regexp

--- app/validation/schemas/loginSchema.js
+++ app/validation/schemas/loginSchema.js
@@ -4,5 +4,5 @@ module.exports = Joi.object({
   // eslint-disable-next-line no-useless-escape
   mail: Joi.string().pattern(/^[\w\-\+]+(\.[\w\-]+)*@[\w\-]+(\.[\w\-]+)*\.[\w\-]{2,4}$/).required(),
   password: Joi.string(),
-// .pattern(/^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$/).required(),
+// .pattern(/^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*[^"]$/).required(),
 }).required();
