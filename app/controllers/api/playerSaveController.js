b1388f127f7d23c80d10641de26139a0ee4d56f9
AgathePons <ponsagathe37@gmail.com>
2022-04-06 13:27:59 +0200

    Remove some comments from controller

--- app/controllers/api/playerSaveController.js
+++ app/controllers/api/playerSaveController.js
@@ -1,13 +1,15 @@
-/* eslint-disable max-len */
 const debug = require('debug')('controller:playerSave');
 const save = require('../../services/buildSave');
-// const { ApiError } = require('../../helpers/errorHandler');
+const { ApiError } = require('../../helpers/errorHandler');
 
 module.exports = {
   async getSave(req, res) {
     debug(`getSave for player: ${req.decoded.id} ${req.decoded.username}`);
     const playerId = req.decoded.id;
     const playerSave = await save.buildSave(playerId);
+    if (!playerSave) {
+      throw ApiError('PlayerSave build error', { statusCode: 500 });
+    }
     res.json(playerSave);
   },
 };
