4c3b828666686f4ec74809c97f54410e5e41ba5f
AgathePons <ponsagathe37@gmail.com>
2022-04-06 14:21:10 +0200

    call save service in login controller

--- app/controllers/api/playerAccountController.js
+++ app/controllers/api/playerAccountController.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const playerAccountDataMapper = require('../../models/playerAccount');
+const save = require('../../services/buildSave');
 const { ApiError } = require('../../helpers/errorHandler');
 
 module.exports = {
@@ -19,28 +20,30 @@ module.exports = {
     debug('create called', req.body);
     const existingUsername = await playerAccountDataMapper.findByUsername(req.body.username);
     const existingMail = await playerAccountDataMapper.findByMail(req.body.mail);
-
+    // check if username already exists
     if (existingUsername) {
       throw new ApiError(`${req.body.username} already exists in db for username`, { statusCode: 400 });
     }
+    // check if mail already exists
     if (existingMail) {
       throw new ApiError(`${req.body.mail} already exists in db for mail`, { statusCode: 400 });
     }
+    // check if password matches with passwordConfirm
     if (req.body.password !== req.body.passwordConfirm) {
       throw new ApiError('password confirm failed', { statusCode: 418 });
     }
-
+    // encrypt password
     const salt = await bcrypt.genSalt(5);
     const encryptedPassword = await bcrypt.hash(req.body.password, salt);
-
+    // build new account data
     const newAccount = {
       username: req.body.username,
       mail: req.body.mail,
       password: encryptedPassword,
     };
-
+    // insert in db
     const newPlayer = await playerAccountDataMapper.insert(newAccount);
-    debug('newplayer', newPlayer);
+    // create jwt
     const expireIn = 24 * 60 * 60;
     const payload = {
       id: newPlayer.id,
@@ -53,9 +56,13 @@ module.exports = {
         expiresIn: expireIn,
       },
     );
+    // set headers Authorization with jwt
     res.header('Authorization', `Bearer ${token}`);
     debug('token:', token);
-    return res.status(200).json({ logged: true, token });
+    // build save
+    debug('new player', newPlayer);
+    const playerSave = await save.buildSave(newPlayer.id);
+    return res.status(200).json({ logged: true, token, playerSave });
   },
 
   async update(req, res) {
@@ -106,14 +113,17 @@ module.exports = {
   },
   async login(req, res) {
     const player = await playerAccountDataMapper.findByMail(req.body.mail);
+    // check if email exists
     if (!player) {
       throw new ApiError('This usermail / password does not exists', { statusCode: 403 });
     }
+    // check if password is valid
     const validPassword = await bcrypt.compare(req.body.password, player.password);
     if (!validPassword) {
       throw new ApiError('This usermail / password does not exists', { statusCode: 403 });
     }
     delete player.password;
+    // create jwt
     const expireIn = 24 * 60 * 60;
     const payload = {
       id: player.id,
@@ -126,8 +136,10 @@ module.exports = {
         expiresIn: expireIn,
       },
     );
+    // set headers Authorization with jwt
     res.header('Authorization', `Bearer ${token}`);
     debug('token:', token);
-    return res.status(200).json({ logged: true, token });
+    const playerSave = await save.buildSave(player.id);
+    return res.status(200).json({ logged: true, token, playerSave });
   },
 };
