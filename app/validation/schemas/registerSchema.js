3b501db20898e5e3b2d4f2360e894c46d462941c
AgathePons <ponsagathe37@gmail.com>
2022-04-07 15:22:17 +0200

    re-comment the joi regex validation for password

--- app/validation/schemas/registerSchema.js
+++ app/validation/schemas/registerSchema.js
@@ -4,7 +4,8 @@ module.exports = Joi.object({
   username: Joi.string().pattern(/[A-Za-z0-9]{3,15}$/).required(),
 
   password: Joi.string().pattern(/^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*[^"]$/).required(),
-  passwordConfirm: Joi.string().pattern(/^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*[^"]$/).required(),
+  passwordConfirm: Joi.string(),
+  // .pattern(/^(?=.{10,}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*[^"]$/).required(),
   // eslint-disable-next-line no-useless-escape
   mail: Joi.string().pattern(/^[\w\-\+]+(\.[\w\-]+)*@[\w\-]+(\.[\w\-]+)*\.[\w\-]{2,4}$/).required(),
 
