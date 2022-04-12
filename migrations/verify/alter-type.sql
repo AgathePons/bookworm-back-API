df3db67cf4cae2796691732d570c3b5879c90b65
Yoan F <feurtet.yoan@gmail.com>
2022-04-12 14:22:28 +0200

    alter type

---
+++ migrations/verify/alter-type.sql
@@ -0,0 +1,8 @@
+-- Verify bookworm:alter-type on pg
+SELECT clic_flat_value FROM generator WHERE false;
+
+SELECT idle_flat_value FROM generator WHERE false;
+
+SELECT starting_cost FROM generator WHERE false;
+
+ROLLBACK;
